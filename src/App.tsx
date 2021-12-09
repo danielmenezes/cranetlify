01bb1eaf65b3241c444057b86ca21084471c45df
danielmenezes <daniel.menezes.md@gmail.com>
2021-12-09 16:26:26 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React with Rocketseat
-          Nova branch
+          Nova
         </a>
       </header>
     </div>
